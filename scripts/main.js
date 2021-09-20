fa4744f2bdd23c19ae66683dc1855c4c025f0e24
KostyaKorshunov <korshunovk@gmail.com>
2021-09-20 23:05:49 +0300

    fix: done

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 
-	const el_works = document.querySelector("#RightCol")
+//	const el_works = document.querySelector("#RightCol")
 
-	el_works.setAttribute("class", "works_cntr");
+//	el_works.setAttribute("class", "wk");
 
 //	alert( el_works.getAttribute("class") );
 
 // adaptiv flex	+-
 // burger		+++ 
+
+
+// Belarusian Research Center for Land Management, Geodesy and Cartography under the State Committee for Property of the Republic of Belarus
